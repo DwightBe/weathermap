9e8f163c540c3c9a8190485cb922de3c52eaa54b
Dwight Benignus <dwightbenignus@gmail.com>
2018-05-07 08:23:35 -0500

    key

--- src/services/openweathermap/index.js
+++ src/services/openweathermap/index.js
@@ -1,6 +1,6 @@
 import qs from 'qs';
 
-const apiToken = '886268d885e2614466725dcf8b9589c5';
+const apiToken = '48fbf0232188005e22958e2769cb04cc';
 const apiUrl = 'http://api.openweathermap.org/data/2.5';
 
 const fetchApi = (endPoint, payload = {}, method = 'get', headers = {}) => {
