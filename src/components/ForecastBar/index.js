e12f7752818f70892ce2745d00fc20372de642ab
Dwight Benignus <dwightbenignus@gmail.com>
2018-05-07 08:31:47 -0500

    styles

--- src/components/ForecastBar/index.js
+++ src/components/ForecastBar/index.js
@@ -9,11 +9,11 @@ class ForecastBar extends React.Component { // eslint-disable-line react/prefer-
     let weathericon = () => {
     switch(this.props.weather.main){
       case 'Clear':
-        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png' />
+        return <img className='weathericon' src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png' />
       case 'Clouds':
-        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-22-256.png' />
+        return <img className='weathericon' src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-22-256.png' />
       case 'Rain':
-        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-31-256.png' />
+        return <img className='weathericon' src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-31-256.png' />
       }
     }
     console.log(weathericon)
@@ -24,9 +24,9 @@ class ForecastBar extends React.Component { // eslint-disable-line react/prefer-
         <div>
           {weathericon()}
         </div>
-        <h3> {this.props.weather.description}</h3>
-        <h2> {this.props.temp}&deg;</h2>
-        <h2> {this.props.humidity}% humidity </h2>
+        <div> {this.props.weather.description}</div>
+        <div> {this.props.temp}&deg;</div>
+        <div> {this.props.humidity}% humidity </div>
       </ContentContainer>
     );
   }
--- src/components/ForecastBar/styles.js
+++ src/components/ForecastBar/styles.js
@@ -3,9 +3,19 @@ import styled from 'styled-components';
 export const ContentContainer = styled.div`
 	margin: 0 auto;
 	text-align: center;
-  width: 500px;
+  width: 400px;
+	height: 280px;
   background-color: #e6f5ff;
+	h2{
+		background-color: #b8c4cc;
+		line-height: 2em;
+	}
+	.weathericon{
+		width: 100px;
+		height: 100px;
+	}
 	@media (max-width: 480px) {
+
 
 	}
 `;
