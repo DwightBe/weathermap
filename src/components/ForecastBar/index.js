e0f104efe98a1f8f8d3eda6e43b6d9887025d30f
Dwight Benignus <dwightbenignus@gmail.com>
2018-05-07 08:22:17 -0500

    styles and icons

--- src/components/ForecastBar/index.js
+++ src/components/ForecastBar/index.js
@@ -6,13 +6,27 @@ class ForecastBar extends React.Component { // eslint-disable-line react/prefer-
     super();
   }
   render() {
+    let weathericon = () => {
+    switch(this.props.weather.main){
+      case 'Clear':
+        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-256.png' />
+      case 'Clouds':
+        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-22-256.png' />
+      case 'Rain':
+        return <img src='https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-31-256.png' />
+      }
+    }
+    console.log(weathericon)
     return (
       <ContentContainer>
         <h2>{this.props.date}</h2>
         <div> {this.props.weather.main}</div>
-        <div> {this.props.weather.description}</div>
-        <div> {this.props.temp}&deg;</div>
-        <div> {this.props.humidity}% humidity </div>
+        <div>
+          {weathericon()}
+        </div>
+        <h3> {this.props.weather.description}</h3>
+        <h2> {this.props.temp}&deg;</h2>
+        <h2> {this.props.humidity}% humidity </h2>
       </ContentContainer>
     );
   }
